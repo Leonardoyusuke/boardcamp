f64fee52aa999f3d92645c149c6724d289c33584
leonardoyusuke <leoyuhirano@gmail.com>
2023-02-10 17:24:58 +0900

    customers fix

--- src/Schema/customersSchema.js
+++ src/Schema/customersSchema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const customersSchema = Joi.object({
     name: Joi.string().min(1).required(),
-    phone: Joi.string().min(10).max(11).required(),
-    cpf: Joi.number().min(11).max(11).required(),
-    birthday: Joi.date().iso().required(),
+    phone: Joi.string().min(10).max(13).required(),
+    cpf: Joi.string().min(11).max(11).required(),
+    birthday: Joi.date().iso().required()
   });
--- src/middleware/customersMiddleware.js
+++ src/middleware/customersMiddleware.js
@@ -3,7 +3,7 @@ import { customersSchema } from "../Schema/customersSchema.js";
 import { customersPost } from "../Controllers/customers.js";
 
 export function validationSchema(req,res,next) {
-    const validation = customersSchema.validate(req.body);
+    const validation = customersSchema.validate(req.body,{abortEarly:true});
     console.log(validation,"validacao")
 
     if (validation.error) {
