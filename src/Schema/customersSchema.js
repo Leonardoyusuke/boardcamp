b3b45f6adce0aa0a40f061c63926a1f133156c16
leonardoyusuke <leoyuhirano@gmail.com>
2023-02-13 19:34:19 +0900

    a

--- src/Schema/customersSchema.js
+++ src/Schema/customersSchema.js
@@ -3,6 +3,6 @@ import Joi from "joi";
 export const customersSchema = Joi.object({
     name: Joi.string().min(1).required(),
     phone: Joi.string().min(10).max(13).required(),
-    cpf: Joi.string().valid("^[0-9]+$").required(),
+    cpf: Joi.string().min(11).max(11).required(),
     birthday: Joi.date().iso().required()
   });
