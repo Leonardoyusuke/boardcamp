271cd66b3420ab41afd94030919e017381116ae4
leonardoyusuke <leoyuhirano@gmail.com>
2023-02-13 17:12:16 +0900

    fix

--- src/Schema/rentalSchema.js
+++ src/Schema/rentalSchema.js
@@ -2,4 +2,6 @@ import Joi from "joi";
 
 export const rentalSchema = Joi.object({
     customerId: Joi.number().min(1).required(),
+    gameId: Joi.number().required(),
+    daysRented: Joi.number().required()
   });
